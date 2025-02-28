bb92c438618540c1746209b7f11ca9b7616fe208
YukihiraDeve <valentinbourpro@gmail.com>
2025-02-28 10:51:31 +0100

    init files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import GalaxyScene from './Galaxy'
+import AdvancedGalaxyScene from './AdvancedGalaxy'
+import './styles.css'
+
+const App = () => {
+
+  
+  return (
+      </div>
+    </>
+  )
+}
+
+root.render(<App />)
